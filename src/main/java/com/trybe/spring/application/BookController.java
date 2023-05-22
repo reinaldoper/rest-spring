180fda3d9ff2868617bcff3ea3138a60dbfbec17
reinaldoper <reinaldoper83@gmail.com>
2023-05-22 16:25:07 -0400

    [reinaldo-api-rest-spring]

--- src/main/java/com/trybe/spring/application/BookController.java
+++ src/main/java/com/trybe/spring/application/BookController.java
@@ -1,6 +1,107 @@
 package com.trybe.spring.application;
 
+import com.trybe.spring.domain.Book;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+import javax.websocket.server.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Path("/api/books")
+
 public class BookController {
 
+  private List<Book> books = new ArrayList<>();
+  /**
+     * Método do desafio.
+     * 
+     */
 
+  @POST
+  @Consumes("application/json") // tipo de dado que é consumido
+  @Produces("application/json") // tipo de dado enviado como resposta
+  public Response add(Book book) {
+    books.add(book);
+    return Response.ok(book).build();
+  }
+
+  /**
+     * Método do desafio.
+     * 
+     */
+
+  @PUT
+  @Consumes("application/json") // tipo de dado que é consumido
+  @Produces("application/json") // tipo de dado enviado como resposta
+  public Response update(Book book) {
+    try {
+      Book boo = books.stream().filter(b -> b.getId().equals(book.getId())).findAny().orElseThrow();
+      boo.setAuthor(book.getAuthor());
+      boo.setName(book.getName());
+      books.add(boo);
+      return Response.ok(book).build();
+
+    } catch (NoSuchElementException e) {
+      return Response.status(404).build();
+    }
+  }
+
+
+  /**
+     * Método do desafio.
+     * 
+     */
+  @DELETE
+  @Path("/{id}")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response remove(@PathParam("id") UUID id) {
+    try {
+      Book book = books.stream().filter(b -> b.getId().equals(id)).findAny().orElseThrow();
+      books.remove(book);
+      return Response.ok(book).build();
+    } catch (NoSuchElementException e) {
+      return Response.status(404).build();
+    }
+  }
+
+  /**
+     * Método do desafio.
+     * 
+     */
+
+  @GET
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response findAll() {
+    return Response.ok(books).build();
+  }
+
+  /**
+     * Método do desafio.
+     * 
+     */
+
+  @GET
+  @Path("/{id}")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response findById(@PathParam("id") UUID id) {
+    try {
+      Book book = books.stream().filter(b -> b.getId().equals(id)).findAny().orElseThrow();
+      return Response.ok(book).build();
+    } catch (NoSuchElementException e) {
+      return Response.status(404).build();
+    }
+  }
 }
--- src/main/java/com/trybe/spring/domain/Book.java
+++ src/main/java/com/trybe/spring/domain/Book.java
@@ -1,6 +1,52 @@
 package com.trybe.spring.domain;
 
+import java.util.UUID;
+
 public class Book {
+  private UUID id;
+
+  private String name;
+
+  private String author;
+
+  /**
+   * Método do desafio.
+   * 
+   */
+
+  public Book(String name, String author) {
+    /**
+     * Método do desafio.
+     * 
+     */
+    this.setId(UUID.randomUUID()); // toda nova instância terá um novo id
+    this.setName(name);
+    this.setAuthor(author);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
 
 }
 
