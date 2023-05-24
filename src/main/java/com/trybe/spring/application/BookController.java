1f2de82f0a5e62becb158eb08be74cb2339698b2
reinaldoper <reinaldoper83@gmail.com>
2023-05-24 16:04:04 -0400

    feat: ajusta bookController, remove, findById

--- src/main/java/com/trybe/spring/application/BookController.java
+++ src/main/java/com/trybe/spring/application/BookController.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
-import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
@@ -23,10 +23,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Path("/api/books")
 public class BookController {
   private List<Book> books = new ArrayList<Book>();
-
-  /**
-   * Método classe.
-   */
 
   /**
    * Método classe.
@@ -93,22 +89,23 @@ public class BookController {
    *
    */
 
-  /*
-   * @PUT
-   * 
-   * @Path("/{id}")
-   * 
-   * @Consumes("application/json")
-   * 
-   * @Produces("application/json") public Response update(@PathParam("id") UUID id,@RequestBody Book
-   * updatedBook) { try { Book book = books.stream().filter(b ->
-   * b.getId().equals(id)).findAny().orElseThrow();
-   * 
-   * 
-   * book.setName(updatedBook.getName()); book.setAuthor(updatedBook.getAuthor());
-   * 
-   * 
-   * return Response.ok(book).build(); } catch (NoSuchElementException e) { return
-   * Response.status(404).build(); } }
-   */
+
+  @PUT
+  @Path("/{id}")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response update(@PathParam("id") UUID id, @RequestBody Book updatedBook) {
+    try {
+      Book book = books.stream().filter(b -> b.getId().equals(id)).findAny().orElseThrow();
+      System.out.println("update" + books);
+      book.setName(updatedBook.getName());
+      book.setAuthor(updatedBook.getAuthor());
+
+
+      return Response.ok(books.get(0)).build();
+    } catch (NoSuchElementException e) {
+      return Response.status(404).build();
+    }
+  }
+
 }
--- src/test/java/com/trybe/spring/application/BookControllerTest.java
+++ src/test/java/com/trybe/spring/application/BookControllerTest.java
@@ -1,5 +1,5 @@
 package com.trybe.spring.application;
-​
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,23 +17,23 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import com.trybe.spring.domain.Book;
-​
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BookControllerTest {
-​
+
   @LocalServerPort
   private int port;
-​
+
   @Autowired
   private TestRestTemplate restTemplate;
-​
+
   @Test
   @DisplayName("add - Cenário de Sucesso")
   void add() {
     Book book = postBook("Teste Add Name", "Teste Add Author");
     Assertions.assertEquals("Teste Add Author", book.getAuthor());
   }
-​
+
   @Test
   @DisplayName("findById - Cenário de Sucesso")
   void findById() {
@@ -43,7 +43,7 @@ class BookControllerTest {
         .getBody();
     Assertions.assertEquals("name findById1", book.getName());
   }
-​
+
   @Test
   @DisplayName("findById - Cenário de Erro - Livro não encontrado (404)")
   void findByIdNotFound() {
@@ -51,7 +51,7 @@ class BookControllerTest {
         HttpMethod.GET, null, Void.class);
     Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
   }
-​
+
   @Test
   @DisplayName("findAll - Cenário de Sucesso")
   void findAll() {
@@ -63,13 +63,13 @@ class BookControllerTest {
     Assertions.assertTrue(books.contains(book1));
     Assertions.assertTrue(books.contains(book2));
   }
-​
+
   @Test
   @DisplayName("update - Cenário de Sucesso")
   void update() {
     Map<String, String> params = new HashMap<>();
     params.put("filter", "updateName1");
-​
+
     Book book = postBook("Post name 1", "Post author 1");
     book.setName("updateName1");
     putBook(book);
@@ -78,7 +78,7 @@ class BookControllerTest {
     Assertions.assertEquals(1, books.size());
     Assertions.assertEquals(book, books.get(0));
   }
-​
+
   @Test
   @DisplayName("update - Cenário de Erro - Livro não encontrado (404)")
   void updateNotFound() {
@@ -86,54 +86,56 @@ class BookControllerTest {
     ResponseEntity<?> response = putBook(book);
     Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
   }
-​
+
   @Test
   @DisplayName("remove - Cenário de Sucesso")
   void remove() {
     Map<String, String> params = new HashMap<>();
     params.put("filter", "deleteName1");
-​
+
     Book book = postBook("deleteName1", "deleteAuthor1");
     List<Book> books = this.restTemplate.exchange(getUrl() + "?filter={filter}", HttpMethod.GET,
         null, new ParameterizedTypeReference<List<Book>>() {}, params).getBody();
     Assertions.assertEquals(1, books.size());
     Assertions.assertEquals(book, books.get(0));
-​
+
     deleteBook(book.getId());
     books = this.restTemplate.exchange(getUrl() + "?filter={filter}", HttpMethod.GET, null,
         new ParameterizedTypeReference<List<Book>>() {}, params).getBody();
     Assertions.assertEquals(0, books.size());
   }
-​
+
   @Test
   @DisplayName("remove - Cenário de Erro - Livro não encontrado (404)")
   void removeNotFound() {
     ResponseEntity<?> response = deleteBook(UUID.randomUUID());
     Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
   }
-​
+
   private String getUrl() {
     return "http://localhost:" + port + "/api/books";
   }
-​
+
   private Book postBook(String name, String author) {
     return this.restTemplate.postForObject(getUrl(), new BookDTO(name, author), Book.class);
   }
-​
+
   private ResponseEntity<?> putBook(Book book) {
     return this.restTemplate.exchange(getUrl() + "/" + book.getId().toString(), HttpMethod.PUT,
         new HttpEntity<>(book), Void.class);
   }
-​
+
   private ResponseEntity<?> deleteBook(UUID id) {
     return this.restTemplate.exchange(getUrl() + "/" + id.toString(), HttpMethod.DELETE, null,
         Void.class);
   }
+
+
   class BookDTO {
-​
+
     public String name;
     public String author;
-​
+
     public BookDTO(String name, String author) {
       this.name = name;
       this.author = author;
